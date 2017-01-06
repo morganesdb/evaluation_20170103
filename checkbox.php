261c25d4563001a7cffe65d760d943885b2c269f
morganesdb <morgane.sidobre34@gmail.com>
2017-01-06 15:49:47 +0100

    Add files via upload

--- checkbox.php
+++ checkbox.php
@@ -1,27 +1,93 @@
 <?php
 
 // Le tableau $_POST['prenom'] contient les valeurs des checkbox cochées
+$fp = fopen("reponse.html", "w+");
 
-foreach($_POST['choix'] as $valeur)
 
-{
 
-  echo '<!DOCTYPE html>'.
+  fputs($fp, '<!DOCTYPE html>
 
-      '<html>'.
 
-      '<head>'.
 
-      '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'.
+      <html>
 
-      '<title>evaluations du 03 janvier 2017 – ville</title>'.
 
-      '<link rel="stylesheet" type="text/css" href="reponse.css">'.
 
-      '</head>'.
+      <head>
 
-      '<body>';
 
-   echo '<p>',"La checkbox, $valeur a été cochée<br>", '<section id=heure>',$heure =date("H:i"),'</section>','</p>';
+
+      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+
+
+      <title>evaluations du 03 janvier 2017 – ville</title>
+
+
+
+      <link rel="stylesheet" type="text/css" href="reponse.css">
+
+
+
+      </head>
+
+
+
+      <body>');
+
+
+
+
+
+foreach($_POST as $key => $value){
+
+
+
+
+
+
+
+   echo $key . '<p>', " response :  $value<br>", '</p>';
+fputs($fp, '<p>La checkbox $valeur a été cochée<br><section id=heure>$heure =date("H:i")</section></p>');
+
+fputs($fp, $valeur);
 
 }
+
+
+
+
+
+
+
+//foreach($_POST['ecrire'] as $value){
+
+
+
+    //echo '<p>',"La box, $value a été ecrite<br>", '<section id=heure>',$heure =date("H:i"),'</section>','</p>';
+
+//fputs($fp, '<p>La box $value a été ecrite<br> <section id=heure>$heure =date("H:i")</section></p>');
+
+  //}
+
+
+
+
+
+/*
+
+
+
+Si l'utilisateur coche Adriana et Candice,
+
+
+
+cet exemple va afficher:
+
+
+
+La checkbox Adriana a été cochée
+
+
+
+La checkbox Candice a été cochée
